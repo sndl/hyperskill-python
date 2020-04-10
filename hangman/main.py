ffb04777ee69373309c6b10dbc684a31069ace0b
Stanislav Sandalnikov <stanislav.sandalnikov@jetbrains.com>
2020-04-10 10:50:16 +0300

    hangman: add game function

--- hangman/main.py
+++ hangman/main.py
@@ -1,24 +1,5 @@
 import random
 
-words = ['python', 'java', 'kotlin', 'javascript']
-chosen_word = random.choice(words)
-letters = set(chosen_word)
-guessed_letters = set()
-tried_letters = set()
-tries = 8
-
-
-def validate(string):
-    if not len(string) == 1:
-        print("You should print a single letter")
-        return False
-
-    if not string.islower():
-        print("It is not an ASCII lowercase letter")
-        return False
-
-    return True
-
 
 def menu():
     while True:
@@ -29,43 +10,61 @@ def menu():
             break
 
 
-def print_word():
-    word = chosen_word
+def game():
+    words = ['python', 'java', 'kotlin', 'javascript']
+    chosen_word = random.choice(words)
+    letters = set(chosen_word)
+    guessed_letters = set()
+    tried_letters = set()
+    tries = 8
 
-    for char in chosen_word:
-        if char not in guessed_letters:
-            word = word.replace(char, '-')
+    def validate(char):
+        if not len(char) == 1:
+            print("You should print a single letter")
+            return False
 
-    print(word)
+        if not char.islower():
+            print("It is not an ASCII lowercase letter")
+            return False
+
+        return True
+
+    def print_word():
+        word = chosen_word
+        for char in chosen_word:
+            if char not in guessed_letters:
+                word = word.replace(char, '-')
+        print(word)
+
+    while tries > 0:
+        print()
+        if len(letters) == 0:
+            print(chosen_word)
+            print("You guessed the word!")
+            print("You survived!")
+            break
+
+        print_word()
+        letter = input("Input a letter: ")
+        if not validate(letter):
+            continue
+
+        if letter in tried_letters:
+            print("You already typed this letter")
+            continue
+        else:
+            tried_letters.add(letter)
+
+        if letter in letters:
+            letters.remove(letter)
+            guessed_letters.add(letter)
+        else:
+            tries -= 1
+            print("No such letter in the word")
+    else:
+        print("You are hanged!")
 
 
 print("H A N G M A N")
 menu()
-
-while tries > 0:
-    print()
-    if len(letters) == 0:
-        print(chosen_word)
-        print("You guessed the word!")
-        print("You survived!")
-        break
-
-    print_word()
-    letter = input("Input a letter: ")
-    if not validate(letter):
-        continue
-
-    if letter in tried_letters:
-        print("You already typed this letter")
-        continue
-    else:
-        tried_letters.add(letter)
-
-    if letter in letters:
-        letters.remove(letter)
-        guessed_letters.add(letter)
-    else:
-        tries -= 1
-        print("No such letter in the word")
-else:
-    print("You are hanged!")
+game()
